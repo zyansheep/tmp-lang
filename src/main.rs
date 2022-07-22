22ca19d3422624bc6d2578913b8980b53d21d267
SimonFJ20 <simonfromjakobsen@gmail.com>
2022-07-22 23:23:41 +0200

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-
 use bevy::prelude::*;
 
+mod objects;
+mod parse;
+
 fn main() {
-    println!("Hello, langjam #0003!");
-    App::new().run();
+	println!("Hello, langjam #0003!");
+	App::new().run();
 }
